322f5ee53217f808c84ab78554344093a1dc2eb9
Palash Bauri <palashbauri1@gmail.com>
2020-03-18 10:42:44 +0530

    fixing chrome not showing numbers #1

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 REPORT_URL = "https://bauripalash.github.io/ncov-19-india/data/report.json";
 
 const parsePage = r => {
-  context = {
-    death: r[r.length - 1]["total_death"],
-    effected: r[r.length - 1]["total_effected"],
-    cured: r[r.length - 1]["total_cured"],
-    states: r[r.length - 1]["total_states"]
-  };
-  table = {
-    t: ""
-  };
+  // context = {
+  var death=  r[r.length - 1]["total_death"];
+  var effected= r[r.length - 1]["total_effected"];
+  var cured= r[r.length - 1]["total_cured"];
+  var states= r[r.length - 1]["total_states"];
+  // };
+  var table = "";
   r.slice(0, r.length - 1).forEach(e => {
-    table[
-      "t"
-    ] += `<tr><td>${e["state"]}</td><td>${e["effected"]}</td><td>${e["recovered"]}</td><td>${e["death"]}</td></tr>`;
+    table += `<tr><td>${e["state"]}</td><td>${e["effected"]}</td><td>${e["recovered"]}</td><td>${e["death"]}</td></tr>`;
   });
   lup = r[r.length - 1]["last_update"];
-  table[
-    "t"
-  ] = `<table><tr><th>States/UT</th><th>Effected</th><th>Recovered</th><th>Death</th></tr>${table["t"]}</table>`;
-  writePage(context, table, lup);
+  table = `<table><tr><th>States/UT</th><th>Effected</th><th>Recovered</th><th>Death</th></tr>${table}</table>`;
+  // console.log(table);
+  writePage(effected , cured , death , states , table, lup);
 };
 
-const writePage = (context, table, lup) => {
-  document.getElementById("e_num").innerText = context["effected"];
-  document.getElementById("r_num").innerText = context["cured"];
-  document.getElementById("d_num").innerText = context["death"];
-  document.getElementById("s_num").innerText = context["states"];
-  document.getElementById("state_table").innerHTML = table["t"];
+const writePage = (effected , cured , death , states , table, lup) => {
+  document.getElementById("e_num").innerHTML = effected;
+  document.getElementById("r_num").innerHTML = cured;
+  document.getElementById("d_num").innerHTML = death;
+  document.getElementById("s_num").innerHTML = states;
+  document.getElementById("state_table").innerHTML = table;
   document.getElementById("lup").innerText = lup;
   // boxes = document.getElementById("boxes").innerHTML;
   // box_template = doT.template(boxes);
